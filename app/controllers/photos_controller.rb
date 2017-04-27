1473c4d4505efe2c8d02aa7e3b70e0861b0a7de7
cpfeiffe <cpfeiffe@users.noreply.github.com>
2017-04-27 16:32:51 -0500

    so many changes

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,47 @@
+class PhotosController < ApplicationController
+
+  def create_row
+    url = params[:the_source]
+    cap = params[:the_caption]
+    new_photo = Photo.new
+    new_photo.source = url
+    new_photo.caption = cap
+    new_photo.save
+    redirect_to("/photos/#{new_photo.id}")
+  end
+
+  def new_form
+    render("photos/new_form.html.erb")
+  end
+
+  def index
+    @list_of_all_my_photos = Photo.all
+    render("photos/index.html.erb")
+  end
+
+  def show
+    the_id = params[:da_id]
+    @my_photo = Photo.find(the_id)
+    render("photos/show.html.erb")
+  end
+
+  def edit_form
+    @my_photo = Photo.find(params[:la_id])
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    my_photo = Photo.find(params[:le_id])
+    my_photo.source = params[:user_url]
+    my_photo.caption = params[:user_caption]
+    my_photo.save
+    redirect_to("/photos/#{my_photo.id}")
+  end
+
+  def destroy_row
+    render("photos/destroy_row.html.erb")
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get("/photos/new", { :controller => "photos", :action => "new_form" })
+get("/create_photo", { :controller => "photos", :action => "create_row" })
+
+get("/photos", { :controller => "photos", :action => "index" })
+get("/photos/:da_id", { :controller => "photos", :action => "show" })
+
+get("/photos/:la_id/edit", { :controller => "photos", :action => "edit_form" })
+get("/update_photo/:le_id", { :controller => "photos", :action => "update_row" })
+
+get("/delete_photo/:da_id", { :controller => "photos", :action => "destroy_row" })
+
+
+
+
 end
